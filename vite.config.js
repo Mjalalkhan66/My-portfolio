b704a8e798b3c701b17a9872d55722e84a80d81d
Mjalalkhan66 <jk3434048@gmail.com>
2025-08-21 14:10:48 +0500

    vite.config modified added - base : My-portfio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  base: "/My-portfolio/",   // 👈 must match your repo name
+  plugins: [tailwindcss()],
 })
